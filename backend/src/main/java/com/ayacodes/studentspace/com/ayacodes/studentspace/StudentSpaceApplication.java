535a919c89cc85a907ec3e1ea6fc3d24dc7275cc
Aya <ayajsaneh@gmail.com>
2025-07-31 19:55:22 +0100

    persistence issues

--- backend/src/main/java/com/ayacodes/studentspace/com/ayacodes/studentspace/StudentSpaceApplication.java
+++ backend/src/main/java/com/ayacodes/studentspace/com/ayacodes/studentspace/StudentSpaceApplication.java
@@ -1,13 +1,25 @@
 package com.ayacodes.studentspace;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication
 public class StudentSpaceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentSpaceApplication.class, args);
+    }
+
+    @Bean
+    public static CommandLineRunner printEndpoints(ApplicationContext ctx) {
+        return args -> {
+            RequestMappingHandlerMapping mapping = ctx.getBean(RequestMappingHandlerMapping.class);
+            mapping.getHandlerMethods().forEach((key, value) -> System.out.println(key));
+        };
     }
 
 }
